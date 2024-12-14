c3140d37460ca10424c9d81761b3d99a7fb137d1
My comp <My comp@DESKTOP-HBDEDEI>
2024-12-14 02:50:39 +0200

    123comm1

--- Program.cs
+++ Program.cs
@@ -1,18 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Додайте SignalR до контейнера служб
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Налаштуйте маршрутизацію для SignalR
 app.MapHub<ll1.Hubs.ChatHub>("/chatHub");
 
-// Додайте маршрут для статичних файлів (HTML, JS тощо)
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Налаштуйте маршрути для різних користувачів
 app.MapGet("/user1", async context => {
     context.Response.ContentType = "text/html";
     await context.Response.SendFileAsync("wwwroot/user1.html");
